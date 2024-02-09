431ccb7f833911899cf42e811fb5cdca3486b8e8
RakibTheDevMosaic <devdreamweaver@gmail.com>
2024-02-10 00:25:54 +0600

    mini program

---
+++ CheckNegOrPosNum/CheckNeggOrPosNum.py
@@ -0,0 +1,12 @@
+# Solution (Using Conditional statement)
+
+# Check if a num is positive , negetive or 0:
+
+num = int(input("Enter a number :"))
+
+if num >0 :
+    print(num,"Number is positive.")
+elif num < 0:
+    print(num,'Number is negetive.')
+else:
+    print(num,'Number is zero.')
---
+++ Km_to_mile/KmToMile.py
@@ -0,0 +1,9 @@
+# Solution-1 (Km to mile)
+
+# ****(1 kilometre = 0.621371 Mile)
+
+Km = int(input('How much kilometres :'))
+
+miles = Km * 0.621371
+
+print("convert km to miles :", miles ,'miles')
