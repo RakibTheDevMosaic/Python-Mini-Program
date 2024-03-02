3fa2e741aae020670910dd12d1f5f9688f019dff
RakibTheDevMosaic <devdreamweaver@gmail.com>
2024-03-03 01:28:00 +0600

    edit

--- ArmStrongNumber/ArmStrongNumber.py
+++ ArmStrongNumber/ArmStrongNumber.py
@@ -1,14 +1,14 @@
 # Solution (Find an armstrong number)
 
 num = int(input('enter a number here :'))
-
+order = len(str(num))
 sum = 0
 
 temp = num
 
 while temp > 0:
     digit = temp % 10
-    cube = digit ** 3
+    cube = digit ** order
     sum = sum + cube
     temp //=10
 
